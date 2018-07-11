fb5617541f3da60ee1075c63da324d878b9c5c02
sofia ongele <sofiaongele@gmail.com>
2018-07-11 16:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Write your code in this file!
 function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
-    'This one is on me!'
+    return 'This one is on me!'
   } else if (2000 < ride <= 2500) {
-    'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.'
   } else if (ride > 2500) {
-    'No can do.'
+    return 'No can do.'
   } else {
     'eek.'
   }
