c9214ace2d20aeb68dbab2057c9628e616adced6
sofia ongele <sofiaongele@gmail.com>
2018-07-11 16:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
     return 'This one is on me!'
-  } else if (2000 < ride < 2500) {
+  } else if (2000 < ride && ride < 2500) {
     return 'I will gladly take your thirty bucks.'
   } else if (ride >= 2500) {
     return 'No can do.'
